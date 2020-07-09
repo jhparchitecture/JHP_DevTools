8c73dbf3ff36e20e9ea7f01bd8044aacf55f4489
gochrisgilmer <37340064+gochrisgilmer@users.noreply.github.com>
2020-07-09 17:50:33 -0500

    Added ErrorExit()

--- Src/JHP_DevTools.cpp
+++ Src/JHP_DevTools.cpp
@@ -18,6 +18,8 @@
 #include	<Commdlg.h>
 #include	<iostream>
 #include	<string>
+#include    <sstream>
+#include    <strsafe.h>
 
 // =============================================================================
 //
@@ -41,6 +43,39 @@ std::string ExtractFileName(const std::string& fullPath)
 {
 	const size_t lastSlashIndex = fullPath.find_last_of("/\\");
 	return fullPath.substr(lastSlashIndex + 1);
+}
+
+void ErrorExit(LPTSTR lpszFunction)
+{
+	// Retrieve the system error message for the last-error code
+
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	// Display the error message and exit the process
+
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%s failed with error %d: %s"),
+		lpszFunction, dw, lpMsgBuf);
+	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+	ExitProcess(dw);
 }
 
 // ~~~~~~~~~~~~~~~
